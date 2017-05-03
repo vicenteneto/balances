e42cf11e51f1bd0291023a110ccd30cc948e61c0
Vicente Neto <sneto.vicente@gmail.com>
2017-05-02 21:53:18 -0300

    Adding Transaction schema

--- src/balances/schemas.clj
+++ src/balances/schemas.clj
@@ -2,11 +2,11 @@
   (:require [schema.core :as s]))
 
 (s/defschema Transaction
-             {:id             Long
-              :account-number s/Int
-              :description    s/Str
-              :amount         s/Num
-              :date           s/Num})
+  {:id             Long
+   :account-number s/Int
+   :description    s/Str
+   :amount         s/Num
+   :date           s/Str})
 
 (s/defschema NewTransaction
-             (dissoc Transfer :id :date))
+  (dissoc Transaction :id))
